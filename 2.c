6e07c4b103843ede9cc92da7621cc8d3b6d31588
user <user>
2019-08-27 21:44:17 +0530

    initial project version

---
+++ 2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the number");
+	scanf("%d",&n)
+	printf("the number is",n);
+	return 0;
+}
---
+++ helloworld.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	int a=n+1;
+	printf("enter the number");
+	scanf("%d",&n)
+	printf("the number is",a);
+	return 0;
+}
